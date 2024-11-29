b98a93c07aa58fa0aebe87f9e29026f432857901
Bereket A. <bereketbeki64@gmail.com>
2024-11-29 19:02:48 +0300

    first commit

---
+++ coffee/src/components/Navbar.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-card>
+    <v-toolbar
+      
+      class="!h-16 !bg-[#7cea72] !border-none sticky"
+    >
+    <button class="text-3xl font-bold hover:scale-105 ml-2 ">Noka Coffee </button>
+
+      
+       <v-spacer></v-spacer>
+        <v-tabs
+        center-active="true"
+          v-model="currentItem"
+          fixed-tabs
+          slider-color="Black"
+        >
+          <v-tab
+
+            v-for="item in items"
+            :key="item"
+            
+          >
+            {{ item }}
+          </v-tab>
+
+        </v-tabs>
+     
+    </v-toolbar>
+
+    <v-tabs-items v-model="currentItem">
+      <v-tab-item
+        v-for="item in items.concat(more)"
+        :key="item"
+        
+      >
+      
+      </v-tab-item>
+    </v-tabs-items>
+  
+  <v-parallax
+    src="https://th.bing.com/th/id/R.c125b21f444fdb5c57fa6d0024ecccae?rik=YiMEODDZAYMInA&pid=ImgRaw&r=0"
+    height="700"
+  >
+    <div class="d-flex flex-column fill-height justify-center align-center text-white">
+      <h1 class="text-7xl font-weight-thin mb-4 border-[1px] shadow-lg border-transparent">
+        <span class="font-extrabold text-[#53e945]"  > Nokka</span> <span> Coffee</span>
+      </h1>
+      <h4 class="subheading text-[#2ec76b] text-3xl"> 
+        Coffee Harvesting And Exporting
+      </h4>
+      <h5 class="w-[500px] text-2xl flex">
+        Nokka Coffee is Located In west Guji Zone Engaged in cofffe harvesting and Producing 
+        for last 10 years and Exporting Coffee to International Maeket
+      </h5>
+      <div class="flex gap-10 m-5">
+        <v-btn size="x-large">Contact Us</v-btn>
+        <v-btn size="x-large">About  Us</v-btn>
+      </div>
+    
+
+    </div>
+  </v-parallax>
+</v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      currentItem: 'tab-Web',
+      items: [
+        'Home', 'Gallery ', 'About Us', 'Contact Us', 'Servies',
+      ],
+    
+    }),
+
+    methods: {
+      addItem (item) {
+        const removed = this.items.splice(0, 1)
+        this.items.push(
+          ...this.more.splice(this.more.indexOf(item), 1),
+        )
+        this.more.push(...removed)
+        this.$nextTick(() => { this.currentItem = 'tab-' + item })
+      },
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ coffee/src/views/home.vue
@@ -0,0 +1,35 @@
+<template>
+   <v-parallax
+    src="https://th.bing.com/th/id/R.c125b21f444fdb5c57fa6d0024ecccae?rik=YiMEODDZAYMInA&pid=ImgRaw&r=0"
+    height="700"
+  >
+    <div class="d-flex flex-column fill-height justify-center align-center text-white">
+      <h1 class="text-7xl font-weight-thin mb-4 border-[1px] shadow-lg border-transparent">
+        <span class="font-extrabold text-[#53e945]"  > Nokka</span> <span> Coffee</span>
+      </h1>
+      <h4 class="subheading text-[#2ec76b] text-3xl"> 
+        Coffee Harvesting And Exporting
+      </h4>
+      <h5 class="w-[500px] text-2xl flex">
+        Nokka Coffee is Located In west Guji Zone Engaged in cofffe harvesting and Producing 
+        for last 10 years and Exporting Coffee to International Maeket
+      </h5>
+      <div class="flex gap-10 m-5">
+        <v-btn size="x-large">Contact Us</v-btn>
+        <v-btn size="x-large">About  Us</v-btn>
+      </div>
+    
+
+    </div>
+  </v-parallax>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
